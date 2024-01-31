d2cbb8df45ee5b605cb867a41653c3f05690fcc7
edocico <138378767+edocico@users.noreply.github.com>
2024-01-31 11:17:09 +0100

    add some routes

--- src/router.js
+++ src/router.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Homepage from "./views/Homepage.vue";
+import Contacts from "./views/Contact.vue";
+import DoctorsIndex from "./views/doctors/Index.vue";
+import DoctorsShow from "./views/doctors/Show.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -8,6 +11,22 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: Homepage,
+    },
+    {
+      path: "/contatti",
+      name: "contacts",
+      component: Contacts,
+    },
+    {
+      path: "/medici",
+      name: "doctors.index",
+      component: DoctorsIndex,
+    },
+    {
+      path: "/medici/:slug",
+      name: "doctors.show",
+      props: true,
+      component: DoctorsShow,
     },
   ],
 });
