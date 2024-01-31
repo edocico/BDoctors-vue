bc5313af2be9a9a1b75ec0fe311b6699290da283
edocico <138378767+edocico@users.noreply.github.com>
2024-01-31 10:54:29 +0100

    add homepage route

--- src/router.js
+++ src/router.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Homepage from "./views/Homepage.vue";
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Homepage,
+    },
+  ],
 });
 
 export { router };
