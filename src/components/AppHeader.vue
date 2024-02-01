9410e3fe8e953453e23e6096beb7c5db367e9547
edocico <138378767+edocico@users.noreply.github.com>
2024-02-01 11:54:17 +0100

    fix header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,33 +4,57 @@
       <nav class="nav-bar">
         <div class="d-flex justify-content-between align-items-center">
           <div class="logo-home">
-            <router-link :to="{ name: 'home' }" class="decoration-none text-light">
-              <img class="logo-full-header" src="../assets/logo-white.png" alt="BDoctors">
+            <router-link
+              :to="{ name: 'home' }"
+              class="decoration-none text-light"
+            >
+              <img
+                class="logo-full-header"
+                src="../assets/logo-white.png"
+                alt="BDoctors"
+              />
             </router-link>
           </div>
           <div class="menu-item">
             <ul class="d-flex align-items-center gap-4">
               <li class="header-item">
-                <router-link :to="{ name: 'doctors.index' }" class="decoration-none text-light">I nostri
-                  medici</router-link>
-              </li>
-              <li class="header-item">
-                <router-link :to="{ name: 'contacts' }" class="decoration-none text-light">Contattaci</router-link>
-              </li>
-              <li class="header-item" v-if="!authenticated">
-                <a
+                <router-link
+                  :to="{ name: 'doctors.index' }"
                   class="decoration-none text-light"
-                  href="http://127.0.0.1:8000/login"
-                  >Accedi (icona)</a
+                  >I nostri medici</router-link
                 >
               </li>
+              <li class="header-item">
+                <router-link
+                  :to="{ name: 'contacts' }"
+                  class="decoration-none text-light"
+                  >Contattaci</router-link
+                >
+              </li>
+
               <div class="dropdown">
-                <a class="decoration-none text-light header-item" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a
+                  class="decoration-none text-light header-item"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
                   <span>profilo dottore (icon)</span>
                 </a>
                 <ul class="dropdown-menu" v-if="!authenticated">
-                  <li><a class="dropdown-item" href="http://127.0.0.1:8000/login">Login</a></li>
-                  <li><a class="dropdown-item" href="http://127.0.0.1:8000/register">Register</a></li>
+                  <li>
+                    <a class="dropdown-item" href="http://127.0.0.1:8000/login"
+                      >Login</a
+                    >
+                  </li>
+                  <li>
+                    <a
+                      class="dropdown-item"
+                      href="http://127.0.0.1:8000/register"
+                      >Register</a
+                    >
+                  </li>
                   <!-- <li><a class="dropdown-item" href="#">Logout</a></li> -->
                 </ul>
               </div>
@@ -64,22 +88,21 @@ header {
 
 .dropdown-menu {
   background-color: #43762b;
-  
+
   .dropdown-item {
     color: white;
     font-weight: 600;
   }
 
   .dropdown-item:hover {
-      color: #b0e29d;
-      background-color: #43762b;
-    }
+    color: #b0e29d;
+    background-color: #43762b;
+  }
 }
 
 .logo-full-header {
   width: 150px;
   width: 150px;
-  
 }
 
 .header-item {
