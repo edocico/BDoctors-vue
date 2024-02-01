14e50bcc6a272639717be9947039e680688b238d
Federico-Maiocchi <maiocchifederico@gmail.com>
2024-02-01 17:07:04 +0100

    ADD: v-model in views/doctors/Message.vue

--- src/views/doctors/Message.vue
+++ src/views/doctors/Message.vue
@@ -2,70 +2,6 @@
   <section class="message">
     <h1>Manda un messaggio</h1>
     <div class="container">
-      <form action="" method="POST">
-        <div class="d-flex gap-5 mb-2">
-          <div>
-            <div class="label-cust">
-              <label for="name">Inserisci nome:</label>
-            </div>
-            <input
-              class="input-cust"
-              type="text"
-              placeholder="Francesco"
-              name="name"
-            />
-          </div>
-          <div>
-            <div class="label-cust">
-              <label for="surname">Inserisci cognome:</label>
-            </div>
-            <input
-              class="input-cust"
-              type="text"
-              placeholder="Rossi"
-              name="surname"
-            />
-          </div>
-          <div>
-            <div class="label-cust">
-              <label for="surname">Inserisci cellulare:</label>
-            </div>
-            <input
-              type="tel"
-              name="phone number"
-              id="phone number"
-              pattern="[0-9]{10}"
-              required
-            />
-          </div>
-        </div>
-        <div class="mb-2">
-          <div class="label-cust">
-            <label for="name">Inserisci mail:</label>
-          </div>
-          <input
-            class="input-cust"
-            type="email"
-            name="Inserisci la tua mail"
-            id="email"
-            placeholder="francescorossi@gmail.com"
-          />
-        </div>
-        <div class="mb-2">
-          <div class="label-cust mb-2">
-            <label for="name">Scrivi messaggio:</label>
-          </div>
-          <textarea
-            class="input-cust"
-            name="message"
-            id=""
-            cols="100"
-            rows="10"
-            placeholder="messaggio"
-          ></textarea>
-        </div>
-        <input class="btn-cust" type="submit" value="Invia" />
-      </form>
         <form action="" method="POST">
               <div class="d-flex gap-5  mb-2 ">
                 <div>
@@ -93,7 +29,8 @@
                   </div>
                   <input class="input-cust " type="email" name="Inserisci la tua mail" id="email" placeholder="francescorossi@gmail.com" v-model="emailParam">
               </div>
-              <div class="mb-2">
+              <div class="mb-2
+              ">
                   <div class="label-cust mb-2">
                       <label for="name">Scrivi messaggio:</label>
                   </div>
