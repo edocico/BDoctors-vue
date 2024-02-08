2114fc8c21dbe3079cc841a4f14331144fbe7ca1
Federico-Maiocchi <maiocchifederico@gmail.com>
2024-02-08 19:15:06 +0100

    FIX: style card in DoctorCard.vue and Index.vue

--- src/views/doctors/Index.vue
+++ src/views/doctors/Index.vue
@@ -1,48 +1,57 @@
 <template>
-  <!-- <button class="btn btn-primary" :click="fetchAllDoctors()"> -->
-
-  <!-- </button> -->
-  <main>
-    <section class="search-bar bg-middle-green">
-      <div class="">
-        <div class="dropdown">
-          <a
-            class="decoration-none text-light header-item"
-            href="#"
-            role="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            <font-awesome-icon icon="fa-solid fa-user" class="icon fs-3" />
-            <span class="d-none d-md-inline-block"> Specializzazioni </span>
-          </a>
-          <ul class="dropdown-menu">
-            <li
-              v-for="(specialization, index) in store.specializations"
-              :key="index"
-              @click="axiosDoctors(specialization.id)"
+  <main class="pt-2">
+    <section class="">
+      <div class="container">
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="results text-dark-green">
+            <p class="mb-0" v-if="store.allDoctors.length > 0">
+              {{ store.allDoctors.length }}
+              risultati
+            </p>
+            <p class="mb" v-else-if="store.doctorsPerSpecialization.length > 0">
+              {{ store.doctorsPerSpecialization.length }} risultati
+            </p>
+          </div>
+          <div class="btn bg-middle-green dropdown">
+            <a
+              class="decoration-none text-light header-item"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
             >
-              <RouterLink
-                :to="{ name: 'doctors.index' }"
-                class="text-light decoration-none"
+              <font-awesome-icon icon="fa-solid fa-user" class="icon fs-5 pe-3" />
+              <span class="d-none d-md-inline-block fs-5"> Specializzazioni </span>
+            </a>
+            <ul class="dropdown-menu">
+              <li class=""
+                v-for="(specialization, index) in store.specializations"
+                :key="index"
+                @click="axiosDoctors(specialization.id)"
               >
-                {{ specialization.name }}
-              </RouterLink>
-            </li>
-          </ul>
+                <RouterLink
+                  :to="{ name: 'doctors.index' }"
+                  class="text-light decoration-none"
+                >
+                  {{ specialization.name }}
+                </RouterLink>
+              </li>
+            </ul>
+          </div>
         </div>
+        
       </div>
     </section>
     <section class="search-results">
       <div class="container">
-        <p v-if="store.allDoctors.length > 0">
+        <!-- <p v-if="store.allDoctors.length > 0">
           {{ store.allDoctors.length }}
           risultati
         </p>
         <p v-else-if="store.doctorsPerSpecialization.length > 0">
           {{ store.doctorsPerSpecialization.length }} risultati
-        </p>
-        <div class="card-container d-block mb-2 d-md-flex gap-4">
+        </p> -->
+        <div class="card-container d-block mb-2 d-md-flex gap-5 justify-content-evenly">
           <template v-if="store.allDoctors.length > 0">
             <DoctorCard
               v-for="(doctor, index) in store.allDoctors"
@@ -93,21 +102,7 @@ export default {
     axiosDoctors(id) {
       getDoctors(id);
     },
-    // fetchPerSpecialization(index) {
-    //   (this.store.allDoctors = []),
-    //   console.log(index);
-    //   axios
-    //     .get(`${this.store.BASE_URL}/doctors`, {
-    //       params: {
-    //         specialization_ids: [index],
-    //       },
-    //     })
-    //     .then((res) => {
-    //       console.log(res.data);
-    //       this.store.doctorsPerSpecialization = res.data.results;
-    //       console.log(this.store.doctorsPerSpecialization);
-    //     });
-    // },
+    
   },
   created() {
     this.fetchData();
@@ -139,6 +134,14 @@ main {
     }
   }
 
+  .results {
+    padding: 0px 10px;
+    font-size: 18px;
+    font-weight: 700;
+    border: 5px solid #73b760;
+    border-radius: 99px;
+  }
+
   .dropdown {
     background-color: #73b760;
     line-height: 40px;
@@ -152,7 +155,7 @@ main {
       padding: 10px;
 
       .dropdown-item {
-        font-weight: 600;
+        font-weight: 500;
       }
 
       .dropdown-item:hover {
@@ -172,11 +175,7 @@ main {
       padding: 20px 0px;
 
       .card-container {
-        /* display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        gap: 30px; */
         flex-wrap: wrap;
-        /*  */
       }
     }
   }
